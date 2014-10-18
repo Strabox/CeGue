f273456c54ab0f93a37b27ce3182ad3b5a53cb18
Strabox <pardal.pires@gmail.com>
2014-10-18 02:23:21 +0100

    1

--- GameManager.h
+++ GameManager.h
@@ -184,6 +184,19 @@ class GameManager {
 		glClearColor(0.0, 0.0, 0.0, 0.0);
 		glEnable(GL_DEPTH_TEST); // permite desenhar as coisas por ordem de profundidade
 		/*  initialize viewing values  */
+		GLfloat mat_specular[] = { 1.0, 1.0, 1.0, 1.0 };
+		GLfloat mat_shininess[] = { 80.0 };
+		GLfloat light_position[] = { 0.0, 5.0, 5.0, 0.0 };
+		glClearColor(0.0, 0.0, 0.0, 0.0);
+		glShadeModel(GL_SMOOTH);
+
+		glMaterialfv(GL_FRONT, GL_SPECULAR, mat_specular);
+		glMaterialfv(GL_FRONT, GL_SHININESS, mat_shininess);
+		glLightfv(GL_LIGHT0, GL_POSITION, light_position);
+
+		glEnable(GL_LIGHTING);
+		glEnable(GL_LIGHT0);
+		glEnable(GL_DEPTH_TEST);
 	}
 
 };
