09970bda024653d31e8885befdddfa4cfd3e2aec
unknown <ist176102@LAB6P3.rnl.ist.utl.pt>
2014-11-13 13:46:57 +0000

    rnl

--- Road.h
+++ Road.h
@@ -20,6 +20,7 @@ public:
 		texels = (char *)malloc((filelen + 1)*sizeof(char)); // Enough memory for file + \0
 		fread(texels, filelen, 1, fileptr); // Read in the entire file
 		printf("%d\n%s\n", sizeof(texels), (char*)texels);
+		fclose(fileptr);
 	}
 
 	~Road(){}
