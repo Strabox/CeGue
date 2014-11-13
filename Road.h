94029bc973874303a1bb9e939e8faa12efb2676d
carboned4 <slf450x@hotmail.com>
2014-11-13 18:46:36 +0000

    nulidades, depois do jantar é que faço coisas

--- Road.h
+++ Road.h
@@ -20,6 +20,7 @@ public:
 		texels = (char *)malloc((filelen + 1)*sizeof(char)); // Enough memory for file + \0
 		fread(texels, filelen, 1, fileptr); // Read in the entire file
 		printf("%d\n%s\n", sizeof(texels), (char*)texels);
+		giveTextureID(1);
 	}
 
 	~Road(){}
@@ -46,7 +47,8 @@ public:
 		glMaterialfv(GL_FRONT, GL_AMBIENT, mat_ambient);
 		glMaterialfv(GL_FRONT, GL_DIFFUSE, mat_ambient);
 		
-		glTexImage2D(GL_TEXTURE_2D, 0, GL_RGBA, 4, 4, 0, GL_RGBA, GL_UNSIGNED_BYTE, texels);
+		glTexImage2D(GL_TEXTURE_2D, 0, GL_RGB, 4, 4, 0, GL_RGB, GL_UNSIGNED_BYTE, texels);
+		//printf("%d\n%s\n", sizeof(texels), (char*)texels);
 
 		glPushMatrix();
 		glTranslatef(vector.getX(), vector.getY(), vector.getZ());
